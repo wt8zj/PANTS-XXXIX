2b86acc917bc27230a5476dbda185b0c268a2911
Tsai, Wei-Lun <144726700+wt8zj@users.noreply.github.com>
2024-10-22 00:04:16 -0400

    Update generic_task.php

--- generic_task.php
+++ generic_task.php
@@ -1,25 +1,13 @@
 <?php
-// Statcounter code for PANTS
-
-$sc_project = '13048738';
-$sc_security = '8914c5b7';
-$sc_invisible = 1;
-
-// Capture visitor information
-$ip_address = $_SERVER['REMOTE_ADDR'];
-$user_agent = $_SERVER['HTTP_USER_AGENT'];
-$referrer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-$current_url = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
-
-// Send data to Statcounter
-$statcounter_url = "https://c.statcounter.com/{$sc_project}/0/{$sc_security}/1/?ip={$ip_address}&useragent=" . urlencode($user_agent) . "&referer=" . urlencode($referrer) . "&url=" . urlencode($current_url);
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $statcounter_url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
-curl_close($ch);
-
-// Optionally, log the response or any errors for debugging
-// file_put_contents('statcounter_log.txt', $response, FILE_APPEND);
+echo <<<EOD
+<!-- Statcounter code for PANTS https://wt8zj.github.io/PANTS-XXXIX/ on Google Sites (new) -->
+<script type="text/javascript">
+var sc_project=13048738; 
+var sc_invisible=1; 
+var sc_security="8914c5b7"; 
+</script>
+<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>
+<noscript><div class="statcounter"><a title="Web Analytics" href="https://statcounter.com/" target="_blank"><img class="statcounter" src="https://c.statcounter.com/13048738/0/8914c5b7/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
+EOD;
 ?>
